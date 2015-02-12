cb5a61830bd2fcb9a6e6dd9c55be12317df92610
Mark Agrios <magrios@gmail.com>
2015-02-12 12:16:24 -0500

    listed differrent methods in header, not sure what parameters for them however..

--- BinaryTreeHeader.h
+++ BinaryTreeHeader.h
@@ -3,4 +3,26 @@
 
 //the entire header file file
 
+//for InOrder,PreOrder,PostOrder. Output into array and display array
+/***List of Methods, no idea which should contain arguements/return stuff***\
+add(char a)
+addHelper
+remove(char a)
+removeHelper
+searchParent
+PreOrder(array)
+PostOrder(array)
+InOrder(array)
+contains(char a)
+isLeaf()
+oneChild()
+size()
+height()
+isAncestor()
+printLevel()
+
+
+*/
+
+
 #endif
