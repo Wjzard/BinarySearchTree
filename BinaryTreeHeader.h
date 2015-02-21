5760e283f5d72a926d93ea02a49edd0363a89656
Mark Agrios <magrios@gmail.com>
2015-02-21 20:07:36 +0300

    started actually coding, doesn't seem like compiler like the BinarySearchHeader.h, experimental add function in progress

--- BinaryTreeHeader.h
+++ BinaryTreeHeader.h
@@ -1,12 +1,43 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-//the entire header file file
+struct node {
+	struct node left*;
+	struct node right*;
+	char value;
+	
+}
+
+int isLeaf(struct node *head) {
+	if(head->left == NULL && head->right == NULL) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+void add(struct node *head, char value) {
+	
+	if(head == NULL) {
+		head = malloc(sizeof(struct node));
+		head->value = value;
+	}
+	
+	if(value < head->value) {
+		add(head->left, value);
+	}
+	if(value > head->value) {
+		add(head->right, value);
+	}
+
+size++;
+}
+void addHelper(char value) {
+	//add(the main root, value);    ?
+}
 
 //for InOrder,PreOrder,PostOrder. Output into array and display array
 /***List of Methods, no idea which should contain arguements/return stuff***\
-add(char a)
-addHelper
 remove(char a)
 removeHelper
 searchParent
@@ -14,15 +45,14 @@ PreOrder(array)
 PostOrder(array)
 InOrder(array)
 contains(char a)
-isLeaf()
 oneChild()
-size()
-height()
 isAncestor()
 printLevel()
-
-
 */
+
+int size() {
+	return size;
+}
 
 
 #endif
